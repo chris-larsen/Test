52e976e57a584ac91c25b6b0c2cbb2515e3bb460
chris-larsen <clarsen.ce@gmail.com>
2020-11-30 14:46:29 -0800

    reinit

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var randomLetter, t1, t2;
+var inProgress = 0
 
 $("body").keypress(function(event) {
-    if (event.key === "Enter") {
+    if (event.key === "Enter" && inProgress == 0) {
         callGame();
     }
     if (event.key === randomLetter) {
@@ -16,7 +17,7 @@ $("body").keypress(function(event) {
 function callGame() {
     var i = Math.floor(Math.random() * 26);
     randomLetter = letters[i];
-
+    inProgress = 1
 
     // 3,2,1 countdown
     setTimeout(function () {
@@ -43,4 +44,5 @@ function endGame() {
     var msg = Math.round(t2 - t1) + " milliseconds"
     $("h2").css("color", "red");
     $("h2").text(msg);
+    inProgress = 0
 }
